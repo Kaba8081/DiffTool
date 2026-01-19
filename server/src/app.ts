1f54fa1ba6b899bf09d31d4d4b9b46d3b1704729
Kacper Baum <Kaba8081@gmail.com>
2026-01-19 15:15:08 +0100

    Initial project setup

---
+++ server/src/app.ts
@@ -0,0 +1,15 @@
+import type e = require("express");
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (_req: e.Request, res: e.Response) => {
+  res.json({ status: "ok" });
+});
+
+export default app;
